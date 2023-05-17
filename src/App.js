dc96b9cd9dc3ee87d964aaf66858933882cd81ac
Kartikeya Pophali <kartikeyapophali@gmail.com>
2023-05-17 12:33:41 +0530

    chore: add useEffect summary in App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // useEffect - When does it run?
+  //  -> if no dependencies (not even empty [] brackets), runs every time AFTER the component is reloaded
+  //  -> if empty [] brackets for dependencies, runs only once AFTER the component loads for the first time
+  //  -> if dependencies provided in [], runs only when the dependencies change AFTER the component is reloaded
+  // useEffect cleanup function (check Login.js) - When does it run?
+  //  -> runs before every useEffect fn execution - except before first time execution
+  //  -> runs after component is unloaded as well
+
   useEffect(() => {
     const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
     if (storedUserLoggedInInformation === "1") {
